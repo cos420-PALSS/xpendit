1632c8b7799e0270b1ab78f38741db78ce8ab6d9
Matthew Prescott <matthew.prescott97@gmail.com>
2019-03-09 16:38:22 -0500

    added navigation functionality

--- xpendit_app/lib/main.dart
+++ xpendit_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+int pageIndex = 0;
+
 void main() => runApp(XpenditApp());
 
 class XpenditApp extends StatelessWidget {
@@ -12,76 +14,172 @@ class XpenditApp extends StatelessWidget {
       DeviceOrientation.portraitDown,
     ]);
     return new MaterialApp(
-      theme: ThemeData(fontFamily: 'Open Sans'),
-      title: "Xpendit",
-      debugShowCheckedModeBanner: false,
-      home: Home()
-    );
-
+        theme: ThemeData(fontFamily: 'Open Sans'),
+        title: "Xpendit",
+        debugShowCheckedModeBanner: false,
+        home: Home());
   }
 }
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
         backgroundColor: const Color(0xff2b2b2b),
-        body: new Title(),
-        bottomNavigationBar: NavBar()
-    );
+        body: Container(
+          //color: Colors.blue,
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, left: 10.0),
+                child: new Text("Home",
+                    style: new TextStyle(
+                        fontSize: 30.0, color: const Color(0xfff4f4f4))),
+              ),
+              new Title()
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavBar());
   }
 }
 
-class Groups extends StatelessWidget{
+class Groups extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new Scaffold(
-        backgroundColor: const Color(0xff9b42f4),
-        body: new Title(),
-        bottomNavigationBar: NavBar()
-    );
+        backgroundColor: const Color(0xff2b2b2b),
+        body: Container(
+          //color: Colors.blue,
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, left: 10.0),
+                child: new Text("Groups",
+                    style: new TextStyle(
+                        fontSize: 30.0, color: const Color(0xfff4f4f4))),
+              ),
+              new Title()
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavBar());
   }
 }
 
-class Title extends StatelessWidget{
+class ShoppingList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        backgroundColor: const Color(0xff2b2b2b),
+        body: Container(
+          //color: Colors.blue,
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, left: 10.0),
+                child: new Text("Shopping List",
+                    style: new TextStyle(
+                        fontSize: 30.0, color: const Color(0xfff4f4f4))),
+              ),
+              new Title()
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavBar());
+  }
+}
+
+class Balance extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        backgroundColor: const Color(0xff2b2b2b),
+        body: Container(
+          //color: Colors.blue,
+          child: new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, left: 10.0),
+                child: new Text("Balance",
+                    style: new TextStyle(
+                        fontSize: 30.0, color: const Color(0xfff4f4f4))),
+              ),
+              new Title()
+            ],
+          ),
+        ),
+        bottomNavigationBar: NavBar());
+  }
+}
+
+class Title extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      margin: const EdgeInsets.only(top: 40, right: 40),
-      alignment: Alignment.topRight,
-      child: new Text("Xpendit", style: new TextStyle(fontSize:45.0, color: const Color(0xfffc7eb7)),),
+      margin: const EdgeInsets.only(top: 30, right: 10),
+      //alignment: Alignment.topRight,
+      child: new Text(
+        "Xpendit",
+        style: new TextStyle(fontSize: 45.0, color: const Color(0xfffc7eb7)),
+      ),
     );
   }
 }
 
-
 //navigation bar class
-class NavBar extends StatefulWidget{
+class NavBar extends StatefulWidget {
   @override
   _NavBar createState() => _NavBar();
 }
-class _NavBar extends State<NavBar>{
-  int pageIndex = 0; //default page
+
+class _NavBar extends State<NavBar> {
+  //int pageIndex = 0; //default page
 
   @override
   Widget build(BuildContext context) {
     return new Theme(
       data: Theme.of(context).copyWith(
-        // sets the background color of the `BottomNavigationBar`
+          // sets the background color of the `BottomNavigationBar`
           canvasColor: const Color(0xff3b3a43),
           // sets the active color of the `BottomNavigationBar` if `Brightness` is light
           primaryColor: const Color(0xff00ffee),
-          textTheme: Theme
-              .of(context)
-              .textTheme
-              .copyWith(caption: new TextStyle(color: const Color(0xfff4f4f4)))), // sets the inactive color of the `BottomNavigationBar`
+          textTheme: Theme.of(context).textTheme.copyWith(
+              caption: new TextStyle(
+                  color: const Color(
+                      0xfff4f4f4)))), // sets the inactive color of the `BottomNavigationBar`
       child: new BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         //changes page index on bar
         currentIndex: pageIndex,
-        onTap: (int index){
+        onTap: (int index) {
           setState(() {
             pageIndex = index;
+            if (index == 0) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home()),
+              );
+            } else if (index == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Groups()),
+              );
+            } else if (index == 2) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ShoppingList()),
+              );
+            } else if (index == 3) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Balance()),
+              );
+            }
           });
         },
         //nav bar items
